1e04cc686af464acf6594789460d94284606b0cb
Lopeslucas <lucaslopes_51@hotmail.com>
2025-01-19 21:36:31 -0300

    alteracao para deploy - vercel

--- api/app.py
+++ api/app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return jsonify(message="Hello, World!")
 
+# Vercel espera que a variável 'app' seja exportada diretamente.
 if __name__ == "__main__":
     app.run()
+
+# Para que a Vercel consiga usar a função corretamente, exportamos 'app' aqui.
+# Vercel busca essa variável para executar o servidor no ambiente serverless.
+handler = app
