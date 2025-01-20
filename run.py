c07bb6a99a8dde87c9c05fe1e36e5c0a34fcc6cc
Lopeslucas <lucaslopes_51@hotmail.com>
2025-01-19 23:10:05 -0300

    alteracao para deploy - vercel

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
-# run.py
-from app import create_app  # Importando a função para criar o app
+from app import create_app
 
-app = create_app()  # Criando a instância do app
+app = create_app()
+
+# Adiciona log de depuração para verificar se a criação da aplicação ocorre corretamente
+print("Aplicação Flask inicializada com sucesso!")
 
 if __name__ == "__main__":
+    # Isso vai garantir que a aplicação seja executada corretamente
+    print("Executando Flask no modo local...")
     app.run(debug=True)
-
